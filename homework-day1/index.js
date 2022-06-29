2c05d9eac2698440741f230fa637a438ac59d022
WitchElaina <msz13399173387@gmail.com>
2022-06-29 13:07:44 +0800

    Baidu Homework Day1

--- homework-day1/index.js
+++ homework-day1/index.js
@@ -1,12 +1,12 @@
 const ele = document.getElementById("title")
 function changeColor() {
-    if(ele.style.color == "black") {
+    if(ele.style.color == "") {
         ele.style.color = "red";
         console.log("Change to red.");
     }
     else {
-        ele.style.color = "black";
-        console.log("Change to default(black).");
+        ele.style.color = "";
+        console.log("Change to default.");
     }
 }
 
